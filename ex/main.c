5ef842355f5c507d160db410543195cfe34cf98d
daniilshilintsev <1206dan>
2018-08-29 14:56:09 +0300

    Initial Commit

---
+++ ex/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ex
+//
+//  Created by Daniil Shilintsev on 29/08/2018.
+//  Copyright © 2018 Daniil Shilintsev. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
