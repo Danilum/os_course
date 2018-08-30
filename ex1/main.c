a56dc1cd55f51bf3fb50b627437e5d897f2cd97a
Danilum <35626917+Danilum@users.noreply.github.com>
2018-08-31 00:57:57 +0300

    Update main.c
    Sorry for late submission, you can check previous commit, before there were some not useful files like "main.h", don't know why, I will understand it later.

--- ex1/main.c
+++ ex1/main.c
@@ -1,9 +1,27 @@
 //
 //  main.c
-//  ex
+//  ex1
 //
 //  Created by Daniil Shilintsev on 29/08/2018.
 //  Copyright © 2018 Daniil Shilintsev. All rights reserved.
 //
 
-#include "main.h"
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+int main(){
+	int x;
+	float y;
+	double z;
+	x = INT_MAX;
+	y = FLT_MAX;
+	z = DBL_MAX;
+	printf("%ld\n", sizeof(x));
+	printf("%d\n", x);
+	printf("%ld\n", sizeof(y));
+	printf("%f\n" , y);
+	printf("%ld\n", sizeof(z));
+	printf("%f\n", z);
+	return 0;
+}
