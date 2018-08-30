29b5ff37f7ae84937725d0e9e023d0bd66dfff53
Danilum <35626917+Danilum@users.noreply.github.com>
2018-08-31 00:50:22 +0300

    Update main.c
    Sorry for late submission, you can check previous commit, before there were some not useful files like "main.h", don't know why, I will understand it later.

--- ex3/main.c
+++ ex3/main.c
@@ -6,4 +6,22 @@
 //  Copyright © 2018 Daniil Shilintsev. All rights reserved.
 //
 
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int n;
+	scanf("%d", &n);
+    for (int i = 0; i < n; ++i){
+        for (int j = 0; j < 2*n - 1; ++j){
+            if (j < n - i - 1 || j > n + i - 1){
+                printf("%c", ' ');
+            }
+            else{
+                printf("%c", '*');
+            }
+        }
+        printf("\n");
+    }
+	return 0;
+}
